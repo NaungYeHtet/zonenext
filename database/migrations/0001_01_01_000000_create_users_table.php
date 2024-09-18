d544cf578d128b045846e555565996851e984cd4
Naung Ye Htet <naungyehtet717@gmail.com>
2024-09-18 15:40:22 +0630

    update

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -14,9 +14,14 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('phone')->nullable();
+            $table->timestamp('phone_verified_at')->nullable();
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->string('social_id')->nullable();
             $table->string('password');
+            $table->json('preferred_notification_channels')->nullable();
+            $table->timestamp('deactivated_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
